07985f2075803d2e7f709e7adf792fc527508a78
Kennedy <kennedy2000silva@hotmail.com>
2020-08-29 01:06:16 -0300

    move para variaveis de ambiente

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
 	dialect: 'mysql',
-	host: 'kennedy-db.cnukbv9wkw3f.sa-east-1.rds.amazonaws.com',
-	username: 'admin',
-	password: 'awiOg1F8TSLJVXQvjxaN',
-	database: 'pokedex',
+	host: process.env.DB_HOST,
+	username: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_DATABASE,
 }; 
